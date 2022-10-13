b0a99fec7eb552f4b67f92f1f4f197932c1da2ca
zero-cmd-c <115573416+zero-cmd-c@users.noreply.github.com>
2022-10-13 15:19:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['6289513477359'],
-  ['6289513477359'],
+  ['6282124516877'],
+  ['6282124516877'],
   ['6289513477359'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
